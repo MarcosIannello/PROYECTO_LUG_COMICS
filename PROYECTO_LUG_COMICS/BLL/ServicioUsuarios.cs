28b56d5de3f4c132d2608f81af49c1a5bf62c343
JuanPC <jberterre04@gmail.com>
2023-09-10 21:29:01 -0300

    Hash password

--- PROYECTO_LUG_COMICS/BLL/ServicioUsuarios.cs
+++ PROYECTO_LUG_COMICS/BLL/ServicioUsuarios.cs
@@ -18,11 +18,12 @@ namespace BLL
         public int ValidarUsuario(string nombreUsuario, string contraseña)
         {
             ConexionSQL conexionSQL = new ConexionSQL();
-
+            string hashPassword = PasswordHasher.HashPassword(contraseña); // Encripta la contraseña
+           
             SqlParameter[] parametros = new SqlParameter[2];
 
             parametros[0] = new SqlParameter("@NOMBRE_USUARIO", SqlDbType.VarChar, 50) { Value = nombreUsuario.ToUpper() };
-            parametros[1] = new SqlParameter("@PASSWORD", SqlDbType.VarChar, 50) { Value = contraseña };
+            parametros[1] = new SqlParameter("@PASSWORD", SqlDbType.VarChar, 50) { Value = hashPassword };
 
 
             int resultado;
@@ -39,11 +40,13 @@ namespace BLL
             ConexionSQL conexionSQL = new ConexionSQL();
             Random rnd = new Random();
             int id = rnd.Next();
+            string hashPassword = PasswordHasher.HashPassword(password); // Encripta la contraseña
+
 
             SqlParameter[] parametros = new SqlParameter[3];
 
             parametros[0] = new SqlParameter("@NOMBRE_USUARIO", username);
-            parametros[1] = new SqlParameter("@PASSWORD", password);
+            parametros[1] = new SqlParameter("@PASSWORD", hashPassword);
             parametros[2] = new SqlParameter("@id", id);
 
             int resultado = conexionSQL.Escribir("REGISTRAR_USUARIO", parametros);
